05b225ab8350b4a088fc52ea74020da67152a650
hntMichell <nicolasmichellft@gmail.com>
2026-04-07 09:14:17 -0300

    feat: add file upload endpoint

---
+++ services/file_importer.rb
@@ -0,0 +1,32 @@
+require 'csv'
+
+class FileImporter
+  def self.import(file_path)
+    total = 0
+
+    CSV.foreach(file_path, col_sep: "\t", headers: true, encoding: 'ISO-8859-1:UTF-8') do |row|
+      purchaser = Purchaser.find_or_create_by(name: row[0])
+
+      merchant = Merchant.find_or_create_by(
+        name: row[5],
+        address: row[4]
+      )
+
+      item = Item.find_or_create_by(
+        description: row[1],
+        price: row[2].to_f
+      )
+
+      purchase = Purchase.create(
+        purchaser: purchaser,
+        item: item,
+        merchant: merchant,
+        count: row[3].to_i
+      )
+
+      total += item.price * purchase.count
+    end
+
+    total
+  end
+end
